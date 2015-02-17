282e1cf64244029103fced34bc26cc8d91813b4e
saasbook <saasbook@saasbook.(none)>
2015-02-17 06:11:43 -0400

    first deploy to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = '7cfcd030342361f86445058261131cf4286c94c10b4e3f50863dc81d5c63bd9248ba6c4b90ef67322a15ba001d572e32b23b3d59ab8a18b2997c13a1370eb96b'
